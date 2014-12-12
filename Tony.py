31cd11990b9d99f3c976cc7e7200ad0bd8815739
mkim-hj <maruchi.kim@gmail.com>
2014-12-12 06:32:23 -0800

    updating code

--- Tony.py
+++ Tony.py
@@ -9,6 +9,8 @@
 import os
 import sys
 import ParsePy
+import speech_recognition as sr
+import time
 
 ParsePy.APPLICATION_ID = "nfVBFXHTVmVpMr5HHRWl0e3mFk2SDm1hOBoGATxp"
 ParsePy.MASTER_KEY = "i5vvmxea6IBPYRIncjHImfdhoWwUSxYpvRnREx97"
@@ -16,6 +18,19 @@ ParsePy.MASTER_KEY = "i5vvmxea6IBPYRIncjHImfdhoWwUSxYpvRnREx97"
 Stark = "F3:DA:73:20:77:03"
 IronMan = "ED:01:19:83:2D:F4"
 Tony = "F3:FA:BE:3A:4B:96"
+
+r = sr.Recognizer()
+with sr.Microphone(0) as source:                # use the default microphone as the audio source
+    audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
+
+prevTime = 0
+currTime = 0
+
+def setARGB(a, r, g, b):
+	alpha = a
+	redval = r
+	greenval = g
+	blueval = b
 
 while (1):
 	colorQuery = ParsePy.ParseQuery("ColorObject").limit(1).order("createdAt", decending=True)
@@ -34,8 +49,33 @@ while (1):
 	# myarg = " -b " + Stark + " -t random --char-write --handle=0x0011 --value=" + RGB
 	# myarg = " -b " + Stark + " -t random --char-write --handle=0x0011 --value=" + RGB
 
-
 	os.system(mycmd + myarg)
 
-
-
+	# SPEECH RECOGNITION
+	try:
+		speech = r.recognize(audio)
+		print("You said: " + speech)
+		if "Tony" in speech:
+			print "Activated..."
+			if "light on" in speech:
+				print "Turning On"
+				setARGB(1, 220, 220, 220)
+			elif "turn on the lights" in speech:
+				print "Turning On"
+				setARGB(1, 220, 220, 220)
+			elif "light off" in speech:
+				print "Turning Off"
+				setARGB(0, 0, 0, 0)
+			elif "turn off the lights" in speech:
+				print "Turning Off"
+				setARGB(0, 0, 0, 0)
+			elif "dim" in speech:
+				print "Dimming light"
+				alpha = 127
+			elif "dinner" in speech:
+				print "It's time for dinner!"
+				setARGB(1, 255, 220, 80)
+		else:
+			print("Please begin your phrase with 'Tony'")	
+	except LookupError: 
+		print("Could not understand audio")	
